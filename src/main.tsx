534cf937e6a3a8b2d5b75cefb3d2b7746bb09023
Albenoo <albeniskerqeli5@gmail.com>
2023-07-15 01:29:55 +0200

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
-import './index.css';
 import '@fontsource/roboto'; // Defaults to weight 400
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>,
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>,
 );
