be7855057054c944c109158995047e97a721ca48
Albenoo <albeniskerqeli5@gmail.com>
2023-08-18 05:33:14 +0200

    load font using fontsource

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/roboto'; // Defaults to weight 400
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
