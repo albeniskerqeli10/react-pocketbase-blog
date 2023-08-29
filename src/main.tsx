26ae3050df56c425cfd2c8b99bd261a2102cc4c2
Albenoo <albeniskerqeli5@gmail.com>
2023-08-29 19:14:28 +0200

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/roboto/400.css'; // Defaults to weight 400
+import { inject } from '@vercel/analytics';
+inject({
+  mode: 'production',
+});
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <ChakraProvider>
