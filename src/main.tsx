5c19529fb47cff4d9d36774e01b7592564b82b2a
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2023-09-16 13:11:41 +0000

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import '@fontsource/roboto/400.css'; // Defaults to weight 400
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+
+import { inject } from '@vercel/analytics';
+inject({
+  mode: 'production',
+});
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
